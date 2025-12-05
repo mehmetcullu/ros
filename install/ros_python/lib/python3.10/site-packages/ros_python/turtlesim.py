5ea4ca476a1dfc3acbb8d8d460d1bdbcfbf0491f
Mehmet <mehmetculluu@gmail.com>
2025-12-05 17:58:39 +0200

    .

---
+++ install/ros_python/lib/python3.10/site-packages/ros_python/turtlesim.py
@@ -0,0 +1,37 @@
+import rclpy
+from rclpy.node import Node
+from turtlesim.msg import Pose
+
+class TurtlesimKinematics(Node):
+    def __init__(self):
+        super().__init__("turtle_sim_kinematics")
+
+        self.turtle1_pose_sub_ = self.create_subscribtion(Pose, "/turtle1/pose", self.turtle1PoseCallback, 10)
+        self.turtle2_pose_sub_ = self.create_subscribtion(Pose, "/turtle2/pose", self.turtle2PoseCallback, 10)
+
+        self.last_turtle1_pose_= Pose()
+        self.last_turtle2_pose_= Pose()
+    
+    def turtle1PoseCallback(self, msg):
+        self.last_turtle1_pose=msg
+
+    def turtle2PoseCallback(self, msg):
+        self.last_turtle2_pose=msg    
+
+        Tx=self.last_turtle2_pose_.x - self.last_turtle1_pose_.x
+        Ty=self.last_turtle2_pose_.y - self.last_turtle1_pose_.y
+
+        self.get_logger().info("""\n
+                                    Translation Vector turtle1 -> turtle2\n
+                                    Tx: %f \n
+                                    Ty: %f \n""" % (Tx,Ty))
+
+def main():
+    rclpy.init()
+    turtle_sim_kinematics=TurtlesimKinematics()
+    rclpy.spin(turtle_sim_kinematics)
+    turtle_sim_kinematics.destroynode()
+    rclpy.shutdown()
+
+if __name__=='__main__':
+    main()
--- src/ros_python/setup.py
+++ src/ros_python/setup.py
@@ -26,7 +26,8 @@ setup(
         'console_scripts': [
             'publisher = ros_python.publisher:main',
             'subscriber = ros_python.subscriber:main',
-            'parameter = ros_python.parameter:main'
+            'parameter = ros_python.parameter:main',
+            'turtlesim_kinematics = ros_python.turtlesim_kinematics:main'
         ],
     },
 )
